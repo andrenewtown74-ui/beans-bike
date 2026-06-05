57b7203bd469ed435ce2cf1b19a3823820de4682
Neowteown <andrenewtown74@gmail.com>
2026-06-05 17:25:09 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,62 +7,64 @@ const firebaseConfig = {
   appId: "1:813575639636:web:46bc5f60ee57572e3b13c1",
   measurementId: "G-C5K21Q2VLD"
 };
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
+if (typeof firebase !== 'undefined' && !firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+const db = typeof firebase !== 'undefined' ? firebase.firestore() : null;
+
+// Globale Variablen
+let currentLevel = 1;
+let lives = 3;
+let score = 0;
+let levelStartScore = 0;
+let highScore = 0;
+let gameSpeed = 1.5;
 
 let isGameRunning = false;
 let isGameOver = false;
 let isCrashing = false;
+let crashType = '';
 
-
-let canvas, ctx, uiLayer, titleEl, instructionEl, touchControls, fullscreenBtn, headlightBtn;
-let levelData = [];
-let designData = null;
 let worldDistance = 0;
-let nextObstacleIndex = 0;
-let currentLevel = 1;
 let levelDistance = 6000;
+let highestScoredObstacle = -1;
 let finishLineActive = false;
 let finishLineX = 0;
 let isLevelComplete = false;
 let bikeStopped = false;
-let lives = 3;
-let highestScoredObstacle = -1;
+
+let pedalAngle = 0;
 let isHeadlightOn = false;
+let crashAngle = 0;
+let crashBikeLength = 0;
+let gameOverTimer = 0;
+let hasPlayedFanfare = false;
+let hasPlayedSplatSound = false;
+
+let obstacles = [];
 let flyingObjects = [];
-let rainParticles = [];
-let isRaining = false;
+let backgroundElements = [];
+let nextObstacleIndex = 0;
+
+let levelData = [];
+let designData = {};
+
+let player = {
+    targetBikeX: 30,
+    gravity: 0.35,
+    jumpStrength: -6.5,
+    rearWheel: { x: 30, y: 185, defaultX: 30, vy: 0, isJumping: false, isHittingWall: false, onSurface: true, onUphillLiana: false },
+    frontWheel: { x: 90, y: 185, defaultX: 90, vy: 0, isJumping: false, isHittingWall: false, onSurface: true, onUphillLiana: false }
+};
+
+let bikeParts = { rear: null, front: null };
+let beanCrash = { x: 0, y: 0, vx: 0, vy: 0, rotation: 0, isSplat: false };
 
 let keys = { up: false, down: false };
 let touchGas = false;
 let touchBrake = false;
-let isGameRunning = false;
-let isGameOver = false;
-let isCrashing = false;
-let score = 0;
-let levelStartScore = 0;
-let gameSpeed = 1.5;
-let animationFrameId;
+
 let lastTime = 0;
-let pedalAngle = 0;
+let animationFrameId;
 
-let crashType = '';
-let crashAngle = 0;
-let crashBikeLength = 0;
-let gameOverTimer = 0;
-let bikeParts = {
-    rear: { x: 0, y: 0, vx: 0, vy: 0, rot: 0, rotV: 0 },
-    front: { x: 0, y: 0, vx: 0, vy: 0, rot: 0, rotV: 0 }
-};
-let beanCrash = { x: 0, y: 0, vx: 0, vy: 0, rotation: 0, isSplat: false };
-
-let player = {
-    targetBikeX: 30,
-    rearWheel: { x: 30, defaultX: 30, y: 185, vy: 0, isJumping: false, isHittingWall: false, onUphillLiana: false, onSurface: true },
-    frontWheel: { x: 90, defaultX: 90, y: 185, vy: 0, isJumping: false, isHittingWall: false, onUphillLiana: false, onSurface: true },
-    gravity: 0.35,
-    jumpStrength: -6.5
-};
-
-let obstacles = [];
-let backgroundElements = [];
+let canvas, ctx, uiLayer, titleEl, instructionEl, touchControls, fullscreenBtn, headlightBtn;
